52f25ba99e5f4e702af0e21a8fefb86b514c73d2
Anton Leshchynskyi <whitebern321@gmail.com>
2022-01-24 14:45:00 +0200

    Add deep sea fisheries
    Add

---
+++ Deep Sea Fisheries 2020-05-02/Fisheries_Gameplay.sql
@@ -0,0 +1,3 @@
+INSERT OR IGNORE INTO Improvement_ValidTerrains
+		(ImprovementType,		TerrainType)
+VALUES	('IMPROVEMENT_FISHERY',	'TERRAIN_OCEAN');
---
+++ Deep Sea Fisheries 2020-05-02/Fisheries_Text.sql
@@ -0,0 +1,4 @@
+
+INSERT OR REPLACE INTO LocalizedText
+		(Tag, 												Language,	Text)
+VALUES	('LOC_IMPROVEMENT_FISHERY_DESCRIPTION',				'en_US',	'A [ICON_Governor] Governor unique improvement that can be built in a city with a Surveyor Governor with the Aquaculture promotion. Yields 1 [ICON_Food] Food, +1 additional [ICON_Food] Food if adjacent to a sea resource. +1 [ICON_PRODUCTION] Production if in a city with a Surveyor Governor with the Aquaculture promotion. Must be built on a coastal or ocean plot.');
